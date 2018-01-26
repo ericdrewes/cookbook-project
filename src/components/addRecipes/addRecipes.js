0789e1600e09e000fc3f93932cedc7e6f103cf5a
ericdrewes <eric.d.drewes@gmail.com>
2018-01-26 15:30:55 -0600

    update

--- src/components/addRecipes/addRecipes.js
+++ src/components/addRecipes/addRecipes.js
@@ -6,7 +6,7 @@ import "./addRecipes.css";
 
 const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "http://159.89.152.32:3001/"
+    ? "http://159.89.152.32:3001"
     : "http://localhost:3001";
   
 
